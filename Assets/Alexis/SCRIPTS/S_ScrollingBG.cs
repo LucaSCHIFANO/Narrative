a9f6e0e2f9f305228d922bff33a125980b404be2
LucaSchifano <luca.schifano005@gmail.com>
2022-01-26 15:17:18 +0100

    Debut animation lettre

--- Assets/Alexis/SCRIPTS/S_ScrollingBG.cs
+++ Assets/Alexis/SCRIPTS/S_ScrollingBG.cs
@@ -16,7 +16,10 @@ public class S_ScrollingBG : MonoBehaviour
 
     public GameObject phase2;
     public GameObject phase3;
-
+    [SerializeField] private GameObject receivePhase;
+    [SerializeField] private MailBox mail;
+    
+    
     [SerializeField] private List<GameObject> listLetter;
     private int doOnce = 0;
     [SerializeField] private GameObject refListAnswers;
@@ -48,7 +51,18 @@ public class S_ScrollingBG : MonoBehaviour
     IEnumerator Waitfond ()
     {
         yield return new WaitForSeconds(time);
+        
+        phase2.SetActive(false);
         phase3.SetActive(true);
+        receivePhase.SetActive(true);
+        mail.scroll = this;
+        
+        backToOrigin();
+    }
+
+
+    public void showLetter()
+    {
         doOnce += 1;
 
         listLetter[0].SetActive(false);
@@ -78,16 +92,9 @@ public class S_ScrollingBG : MonoBehaviour
             listLetter[2].transform.GetChild(2).GetComponent<Assets.SimpleLocalization.LocalizedText>().Localize2(checkList.answers[value]);
 
         }
-
-
-
-
-        
-
-
-        phase2.SetActive(false);
-        backToOrigin();
     }
+    
+    
 
     public void backToOrigin()
     {
--- Assets/Luca/Script/MailBox.cs
+++ Assets/Luca/Script/MailBox.cs
@@ -3,11 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using Random = UnityEngine.Random;
 
 public class MailBox : MonoBehaviour
 {
     [SerializeField] private Sprite open;
     [SerializeField] private Sprite close;
+    
+    [SerializeField] private List<Sprite> listEnv = new List<Sprite>();
+
+
+    [SerializeField] private GameObject enveloppe;
+    [SerializeField] private GameObject lettre;
+    [SerializeField] private GameObject notif;
+
+
+    public S_ScrollingBG scroll;
+    
+
+    private bool once;
 
     private Image image;
     
@@ -16,14 +31,39 @@ public class MailBox : MonoBehaviour
         image = GetComponent<Image>();
     }
 
-    void Update()
-    {
-        
+    void OnMouseDown(){
+
+        if (!once)
+        {
+            once = true;
+            image.sprite = open;
+            notif.SetActive(false);
+            StartCoroutine(spawnLetter());
+        }
     }
 
-    private void OnMouseDown()
+    IEnumerator spawnLetter()
     {
-        image.sprite = open;
-        Debug.Log("uhfeoirhruhvouvrhprvp");
+        var float1 = Random.Range(0, 3);
+
+        while (float1 == 1)
+        {
+            float1 = Random.Range(0, 3);
+        }
+
+        var imE = enveloppe.GetComponent<Image>();
+
+        imE.sprite = listEnv[float1];
+        
+        enveloppe.transform.DOScale(2, 2);
+        imE.DOFade(1, 1.5f);
+        
+        yield return new WaitForSeconds(1.5f);
+        imE.sprite = listEnv[float1 + 1];
+        
+        yield return new WaitForSeconds(0.5f);
+        lettre.SetActive(true);
+        scroll.showLetter();
+        
     }
 }
